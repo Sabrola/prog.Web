fa03b9bbb790885c19b76f446d1cca721bb7b580
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-08-11 08:54:11 -0300

    Update 02-operador-ternario.js

--- revisao/02-operador-ternario.js
+++ revisao/02-operador-ternario.js
@@ -15,3 +15,18 @@ console.log({media, resultado})
 
 //Usando o operador ternário
 resultado = media >= 6 ? 'APROVADO' : 'REPROVADO'
+//variavel | condição     result true | result false
+
+console.log({media, resultado})
+
+let user = 'guest', msg
+
+if(user === 'admin') msg = 'Login successful.'
+else msg = 'You are not allowed here. Leave.'
+
+console.log({user,msg})
+//converter a estrutura acima em um operardor ternario
+
+msg = user === 'admin' ? 'Login succesful.' : 'You are not allowed here. Leave.'
+
+console.log({user,msg})
